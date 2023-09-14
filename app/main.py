5c18ce90eefc0100662d2a8bcbbb4759e3ff8650
Justin <Justint347@gmail.com>
2023-09-14 09:57:03 -0400

    Connected the Database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from config import settings
+from session import engine
+from base_class import Base
 
-app = FastAPI()
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
-async def root():
-    return {"greeting": "Hello world"}
+def home():
+    return {"msg": "Hello FastAPI🚀"}
+
+
+# from fastapi import FastAPI
+# app = FastAPI()
+
+# @app.get("/")
+# async def root():
+#     return {"greeting": "Hello world"}
